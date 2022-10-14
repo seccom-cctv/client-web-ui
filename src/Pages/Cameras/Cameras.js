f0009805481afbd6f8fb5d874e3f2976761bc8ad
Pedro Monteiro <pmapm@ua.pt>
2022-10-14 17:40:34 +0100

    add list and side by side cameras view

--- src/Pages/Cameras/Cameras.js
+++ src/Pages/Cameras/Cameras.js
@@ -1,27 +1,41 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './Cameras.css'
-import CameraCard from '../../components/CameraCard/CameraCard';
+import './Cameras.css';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList, faSquare, faSqua } from "@fortawesome/free-solid-svg-icons";
+import CamerasList from './components/CamerasList';
+import { useState } from 'react';
+import CamerasTable from './components/CamerasTable';
 
 const Cameras = () => {
+
+    const [showList, setShowList] = useState(true);
+    const [showTable, setShowTable] = useState(false);
+
+    const handleShowList = () => {
+        setShowList(true);
+        setShowTable(false);
+    }
+
+    const handleShowTable = () => {
+        setShowList(false);
+        setShowTable(true);
+    }
+
     return (
         <div className='cameras' data-testid="cameras">
-            <h2 className='cameras-header'>Available Cameras</h2>
-            <Container style={{ width: '100%' }}>
-                <Row style={{marginBottom: '2rem'}}>
-                    <Col><CameraCard image="/cameras/image.jpg" title="House 1" text="Lorem Ipsum" /></Col>
-                    <Col><CameraCard image="/cameras/image2.jpg" title="Office 2" text="Lorem Ipsum" /></Col>
-                    <Col><CameraCard image="/cameras/image2.jpg" title="House 3" text="Lorem Ipsum" /></Col>
-                    <Col><CameraCard image="/cameras/image.jpg" title="Airport 4" text="Lorem Ipsum" /></Col>
-                </Row>
-                <Row>
-                    <Col><CameraCard image="/cameras/image.jpg" title="Office 3" text="Lorem Ipsum" /></Col>
-                    <Col><CameraCard image="/cameras/image2.jpg" title="House 47" text="Lorem Ipsum" /></Col>
-                    <Col><CameraCard image="/cameras/image.jpg" title="House 94" text="Lorem Ipsum" /></Col>
-                    <Col><CameraCard image="/cameras/image2.jpg" title="House 7" text="Lorem Ipsum" /></Col>
-                </Row>
-            </Container>
+            <div className='cameras-header-wrapper'>
+                <h2 className='cameras-header'>Available Cameras</h2>
+                <div className='camera-grid-buttons'>
+                    <Button variant="light" onClick={handleShowList}>
+                        <FontAwesomeIcon icon={faSquare} />
+                        <FontAwesomeIcon style={{ marginLeft: '0.2rem' }} icon={faSquare} />
+                    </Button>
+                    <Button variant="light" onClick={handleShowTable}><FontAwesomeIcon icon={faList} /></Button>
+                </div>
+            </div>
+            <hr className='break-line'></hr>
+            {showList && <CamerasList />}
+            {showTable && <CamerasTable />}
         </div>
     )
 }
