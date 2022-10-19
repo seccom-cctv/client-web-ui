1c97a30c387b8ee90005f08fb3cae5e2d978c352
Pedro Monteiro <pmapm@ua.pt>
2022-10-19 21:07:49 +0100

    add user image and change some design

--- client-web-ui/src/Pages/Settings/Settings.js
+++ client-web-ui/src/Pages/Settings/Settings.js
@@ -1,13 +1,22 @@
 import Notifications from './components/Notifications';
 import TabItems from './components/TabItems';
 import './Settings.css'
+import user from './user.png';
 
 const About = () => {
     return (
         <>
         <div className='about' data-testid="settings">
             <div className="tab-items-wrapper">
-                <TabItems />    
+                <div className="tab-items-account">
+                    <div className="tab-items-account-img-wrapper">
+                        <img src={user} />
+                    </div>
+                    <h4 className="tab-items-account-name">Pedro Monteiro</h4>
+                </div>
+                <div className="tab-items-buttons">
+                <TabItems /> 
+                </div>   
             </div>
             <div className="notifications-wrapper">
                 <Notifications />
--- client-web-ui/src/Pages/Settings/components/TabItems.js
+++ client-web-ui/src/Pages/Settings/components/TabItems.js
@@ -1,11 +1,19 @@
 import './TabItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faKey, faLock, faBell } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const TabItems = () => {
+
+    const [active, setActive] = useState(true);
+
     return (
         <div className="tab-items">
             <ul className="tab-items-list">
-                <li>Profile Settings</li>
-                <li>Notification Settings</li>
+                <li><FontAwesomeIcon icon={faUser} className="tab-item-icon" />Profile</li>
+                <li><FontAwesomeIcon icon={faLock} className="tab-item-icon" />Password</li>
+                <li><FontAwesomeIcon icon={faKey} className="tab-item-icon" />Security</li>
+                <li className={active ? "tab-items-list-item-active" : "tab-items-list-item-active"}><FontAwesomeIcon icon={faBell} className="tab-item-icon " />Notification</li>
             </ul>
         </div>
     )
