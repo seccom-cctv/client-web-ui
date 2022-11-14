4f05fe3c9808abc0ae89946d496725e89bcfc442
zepedrotrigo <josetrigo@ua.pt>
2022-11-14 09:57:35 +0000

    misc changes

--- client-web-ui/src/Pages/Home/Home.js
+++ client-web-ui/src/Pages/Home/Home.js
@@ -1,6 +1,18 @@
 import './Home.css';
+import { useEffect } from "react";
+import { wso2, getWithExpiry } from '../../services/wso2';
 
 const Home = () => {
+
+    useEffect(() => { // on window load -> wso2 authentication
+        //login();
+      }, []);
+    
+    const login = () => {
+    let params = new URLSearchParams(window.location.search);
+    wso2(params.get('code'));
+    }
+
     return (
         <>
         <div className='home' data-testid="home">
